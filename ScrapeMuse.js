2ebeeb389f4563eb8016dc27f64e8023a713025b
Jonathan Morales <jonmorales2.718@gmail.com>
2020-02-12 17:37:41 -0500

    adds functionality to pull sheet music from both pnb and svc sources

--- ScrapeMuse.js
+++ ScrapeMuse.js
@@ -1,9 +1,13 @@
 const puppeteer = require('puppeteer');
-const url = "https://musescore.com/user/168725/scores/1587526";
+// const url = "https://musescore.com/user/168725/scores/1587526"; // png
+// const url = "https://musescore.com/user/12461571/scores/3291706"; // png
+// const url = "https://musescore.com/user/17081446/scores/5256444"; //svg
+// all scores saved after May 2017 = svg
 
 async function run() {
   return new Promise(async(resolve, reject)=> {
     try{
+      //setup stuff
       const browser = await puppeteer.launch({headless: false});
       const webpage = await browser.newPage();
       await webpage.setViewport({
@@ -11,18 +15,27 @@ async function run() {
         height: 1080,
       });
       await webpage.goto(url);
-      // await webpage.waitForNavigation();
+
       await webpage.waitForSelector('.image');
       var sheetsData = await webpage.evaluate(async () => {
         var musicData = [];
         const image = document.querySelector('.image > img');
         const url = image.getAttribute('src');
         const numPages = document.querySelectorAll('.page').length;
+        
+        var scriptsData = Array.from(document.querySelectorAll("script")).filter(x=>x.getAttribute('type')=='application/ld+json');
+        var scriptsInfo  = JSON.parse(scriptsData[0].text);
+        
         musicData.push({"page" : url});
-        var data = {"numberOfPages": [numPages] ,"sheetMusic":musicData}
+        var data = { 
+          "title": scriptsInfo.name,
+          "datePublished": scriptsInfo.datePublished,
+          "numberOfPages": numPages,
+          "url":url
+        };
+
         return data;
       })
-      // await webpage.close();
       await browser.close();
       return resolve(sheetsData);
     }
@@ -30,26 +43,30 @@ async function run() {
       return reject(e);
     }
   })
-  
-  // const linksArr = createLinksArray(sheetsData)
- 
-  // console.log(linksArr);
-
 }
 
-run().then(console.log).catch(console.error);
+run().then(data => {
+  console.log(data);
+  const links = createLinksArray(data);
+  console.log(links);
+}).catch(console.error);
 
-function createLinksArray(arr) {
-  var links = [];
-  const shit = arr.sheetMusic[0].page;
-  //console.log(shit);
+function createLinksArray(data) {
+  let links = [];
+  const link = data.url;
+  links.push(link);
 
-  const shit2 = arr.sheetMusic[0].page;
-  const poop = shit2.replace('0.png', '1.png');
-  //console.log("poop: " + poop);
-  
-  
-  links.push(shit2);
-  links.push(poop);
+  let imageType = "png";
+  if(new Date(data.datePublished) >= new Date(2017,3))
+    imageType = "svg";
+
+  for(var i=1;i<data.numberOfPages;i++){
+    const poop = link.replace(`0.${imageType}`, `${i}.${imageType}`);
+    links.push(poop);
+  }
   return links;
+}
+
+function compareDates(){
+  console.log(new Date())
 }
